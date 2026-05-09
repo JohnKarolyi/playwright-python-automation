3e20f0e0dbe291a0531db40abdbd539dc90b1fe5
JohnKarolyi <JohnKarolyi@users.noreply.github.com>
2026-05-09 13:14:28 +0000

    deploy: b62fb76b09911bd8147443d50b22e33c6b54ca19

--- tests/test_login.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pages.login_page import LoginPage
-
-def test_successful_login(page):
-    login_page = LoginPage(page)
-    login_page.navigate()
-    login_page.login("standard_user", "secret_sauce")
-    assert "inventory.html" in page.url
